d6b6d1381258fff0002c5667f478b41db2e5befb
Igor Yegorov <iegoroff@gmail.com>
2018-06-29 23:37:14 +0300

    version 1.0.1

--- src/Fable.Import.FastMemoize/Fable.Import.FastMemoize.fs
+++ src/Fable.Import.FastMemoize/Fable.Import.FastMemoize.fs
@@ -4,13 +4,4 @@ module Fable.Import.FastMemoize
 open Fable.Core
 
 [<Import("default","fast-memoize"); Emit("$0")>]
-let memoize<'A1, 'T> :('A1 -> 'T) -> ('A1 -> 'T) = jsNative
-
-[<Import("default","fast-memoize"); Emit("$0")>]
-let memoize2<'A1, 'A2, 'T> :('A1 * 'A2 -> 'T) -> ('A1 -> 'A2 -> 'T) = jsNative
-
-[<Import("default","fast-memoize"); Emit("$0")>]
-let memoize3<'A1, 'A2, 'A3, 'T> :('A1 * 'A2 * 'A3 -> 'T) -> ('A1 -> 'A2 -> 'A3 -> 'T) = jsNative
-
-[<Import("default","fast-memoize"); Emit("$0")>]
-let memoize4<'A1, 'A2, 'A3, 'A4, 'T> :('A1 * 'A2 * 'A3 * 'A4 -> 'T) -> ('A1 -> 'A2 -> 'A3 -> 'A4 -> 'T) = jsNative
+let memoize<'a1, 'b> :('a1 -> 'b) -> ('a1 -> 'b) = jsNative
